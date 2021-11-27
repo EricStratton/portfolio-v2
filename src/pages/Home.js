ae38945186555289c048315156370d50f0e62c8f
EricStratton <strattonericj@gmail.com>
2021-11-27 15:54:47 -0800

    add page components

---
+++ src/pages/Home.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import Section from './../components/Section';
+import Column from '../components/Column';
+
+const Home = () => {
+  return (
+    <>
+      <Section>
+        <Column></Column>
+      </Section>
+      <Section>
+        <Column></Column>
+        <Column></Column>
+      </Section>
+      <Section>
+        <Column></Column>
+        <Column></Column>
+        <Column></Column>
+      </Section>
+    </>
+  )
+}
+
+export default Home;
