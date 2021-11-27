4a5c3bb61ff725bf632cf4e0c4f8aa4ae560b221
EricStratton <strattonericj@gmail.com>
2021-11-27 15:02:12 -0800

    add width

--- src/theme/Theme.js
+++ src/theme/Theme.js
@@ -1,5 +1,13 @@
 export const theme = {
   colors: {
-    body: '#f8f8ff',
+    body: '#F0EFF4',
+    primary: '#9E829C',
+    secondary: '#291528',
+    dark: '#3A3E3B',
+    black: '#000000'
+  },
+  width: {
+    max: '1320px',
+    min: '320px'
   }
 };
