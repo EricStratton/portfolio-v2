ffee8019bc93d3fbe0d5cb68b85e1dcce91f4e1d
EricStratton <strattonericj@gmail.com>
2021-11-28 15:04:11 -0800

    add max-width property

--- src/components/Section/SectionElements.js
+++ src/components/Section/SectionElements.js
@@ -2,6 +2,8 @@ import styled  from 'styled-components';
 
 export const SectionContainer = styled.div`
   display: flex;
+  max-width: 76.5em;
   min-height: 31.5em;
+  margin: 0 auto;
   padding: 0 3.5em;
 `;
