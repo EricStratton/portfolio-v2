d1a60a58861e93ddabc34c39f7647de639fb087c
EricStratton <strattonericj@gmail.com>
2021-11-27 16:01:55 -0800

    add sections dir

---
+++ src/sections/ExperienceSection/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Section from './../../components/Section';
+import Column from './../../components/Column';
+
+const ExperienceSection = () => {
+  return (
+    <Section>
+      <Column></Column>
+      <Column></Column>
+    </Section>
+  )
+}
+
+export default ExperienceSection;
