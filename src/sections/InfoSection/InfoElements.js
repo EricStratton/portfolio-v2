49c56477109ace2f4b7a4661e7e361b9665bec2f
EricStratton <strattonericj@gmail.com>
2021-11-28 15:41:40 -0800

    start info section build

---
+++ src/sections/InfoSection/InfoElements.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const InfoWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+`;
+
+export const InfoColumnWrapper = styled.div`
+  display: flex;
+  height: 100%;
+`;
+
+export const InfoHeading = styled.h2`
+  margin-bottom: 1.2em;
+`;
--- src/sections/InfoSection/index.js
+++ src/sections/InfoSection/index.js
@@ -2,13 +2,27 @@ import React from 'react';
 
 import Section from './../../components/Section';
 import Column from './../../components/Column';
+import {
+  InfoWrapper,
+  InfoHeading,
+  InfoColumnWrapper,
+} from './InfoElements';
 
 const InfoSection = () => {
   return (
     <>
       <Section>
-        <Column></Column>
-        <Column></Column>
+        <InfoWrapper>
+        <InfoHeading>About Me</InfoHeading>
+          <InfoColumnWrapper>
+            <Column>
+              <p>Test 1</p>
+            </Column>
+            <Column>
+              <p>Test 2</p>
+            </Column>
+          </InfoColumnWrapper>
+        </InfoWrapper>
       </Section>
     </>
   )
