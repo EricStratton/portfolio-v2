a3a809a66427bb29a2807ff715539b1b40325c93
EricStratton <strattonericj@gmail.com>
2021-11-27 16:31:28 -0800

    add hero

---
+++ src/sections/HeroSection/HeroElements.js
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+
+export const HeroWrapper = styled.div`
+  background: ${props => props.theme.colors.dark};
+`;
---
+++ src/sections/HeroSection/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import Section from '../../components/Section';
+import Column from '../../components/Column';
+import {
+  HeroWrapper
+} from './HeroElements';
+
+const HeroSection = () => {
+  return (
+    <>
+      <HeroWrapper>
+        <Section>
+          <Column>
+        
+          </Column>
+        </Section>
+      </HeroWrapper>
+    </>
+  )
+}
+
+export default HeroSection;
