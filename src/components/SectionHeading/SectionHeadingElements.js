0c3ccf048958d6bef81ae8f30598c4b9c263cd6f
EricStratton <strattonericj@gmail.com>
2021-12-04 15:53:04 -0800

    add star icon

--- src/components/SectionHeading/SectionHeadingElements.js
+++ src/components/SectionHeading/SectionHeadingElements.js
@@ -1,5 +1,18 @@
 import styled from 'styled-components';
+import { IoLogoTableau } from 'react-icons/io5'; 
+
+export const SectionH2Container = styled.div`
+  position: relative;
+`;
 
 export const SectionH2 = styled.h2`
   margin-bottom: 1.2em;
+`;
+
+export const SectionH2Icon = styled(IoLogoTableau)`
+  position: absolute;
+  font-size: 2rem;
+  top: -10px;
+  left: -20px;
+  opacity: 0.1;
 `;
--- src/components/SectionHeading/index.js
+++ src/components/SectionHeading/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 
 import {
+  SectionH2Container,
   SectionH2,
+  SectionH2Icon
 } from './SectionHeadingElements';
 
 const SectionHeading = ({ children }) => {
   return (
     <>
-      <SectionH2>{children}</SectionH2>
+      <SectionH2Container>
+        <SectionH2>{children}</SectionH2>
+        <SectionH2Icon />
+      </SectionH2Container>
     </>
   )
 }
