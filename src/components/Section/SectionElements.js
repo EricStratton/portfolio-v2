8d677c2be4c55ef0c47935f924115deb8dd64dd7
EricStratton <strattonericj@gmail.com>
2021-12-16 11:18:32 -0800

    increase section size & align-items

--- src/components/Section/SectionElements.js
+++ src/components/Section/SectionElements.js
@@ -3,8 +3,9 @@ import styled  from 'styled-components';
 export const SectionContainer = styled.div`
   display: flex;
   max-width: 76.5em;
-  min-height: 31.5em;
+  min-height: 75em;
   margin: 0 auto;
+  align-items: center;
 `;
 
 export const SectionWrapper = styled.div`
