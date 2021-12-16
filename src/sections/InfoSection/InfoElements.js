4325226c2cd594e80a251f3e09c8aba629156557
EricStratton <strattonericj@gmail.com>
2021-12-16 11:02:49 -0800

    add content

--- src/sections/InfoSection/InfoElements.js
+++ src/sections/InfoSection/InfoElements.js
@@ -1,4 +1,6 @@
 import styled from 'styled-components';
+import { BsRecordCircleFill, BsCode } from 'react-icons/bs';
+import { FaMountain } from 'react-icons/fa';
 
 export const InfoWrapper = styled.div`
   display: flex;
@@ -9,4 +11,32 @@ export const InfoWrapper = styled.div`
 export const InfoColumnWrapper = styled.div`
   display: flex;
   height: 100%;
+`;
+
+export const InfoDescriptionWrapper = styled.div`
+  flex-grow: 1;
+`;
+
+export const InfoDescription = styled.p`
+  margin-bottom: 1em;
+  text-indent: ${({indent}) => (indent ? '5%' : 0 )};
+`;
+
+export const InfoIconsWrapper = styled.div`
+  display: flex;
+  justify-content: space-around;
+  font-size: 2.5rem;
+  padding: 0 2em;
+`;
+
+export const RecordIcon = styled(BsRecordCircleFill)`
+  
+`;
+
+export const CodeIcon = styled(BsCode)`
+
+`;
+
+export const MountainIcon = styled(FaMountain)`
+
 `;
--- src/sections/InfoSection/index.js
+++ src/sections/InfoSection/index.js
@@ -4,9 +4,16 @@ import Section from './../../components/Section';
 import Column from './../../components/Column';
 import SectionHeading from './../../components/SectionHeading';
 import Card from './../../components/Card';
+import FadeInUp from './../../components/FadeInUp';
 import {
   InfoWrapper,
   InfoColumnWrapper,
+  InfoDescriptionWrapper,
+  InfoDescription,
+  InfoIconsWrapper,
+  RecordIcon,
+  CodeIcon,
+  MountainIcon,
 } from './InfoElements';
 
 const InfoSection = () => {
@@ -14,14 +21,29 @@ const InfoSection = () => {
     <>
       <Section>
         <InfoWrapper>
-        <SectionHeading delay={100}>About Me</SectionHeading>
+          <SectionHeading delay={100}>About Me</SectionHeading>
           <InfoColumnWrapper>
             <Column third>
               <p>Test 1</p>
             </Column>
             <Column>
               <Card>
-                <p>Test 2</p>
+                <InfoDescriptionWrapper>
+                  <FadeInUp delay={100}>
+                    <InfoDescription indent>In my day-to-day, I build and maintain short-lived SPA’s aimed to generate community engagement for local initiatives using WordPress and Javascript. In my spare time, I work on honing my skills in React.js – currently, I’m working on a headless application using WordPress and React.js to provide a faster, more modern solution to online community engagement.</InfoDescription>
+                  </FadeInUp>
+                  <FadeInUp delay={200}>
+                    <InfoDescription>Outside of development, I have a passion for rock climbing in its various forms. Though I spend most of my time bouldering. One of the great things about living in the Northwest is the easy access to beautiful crags.</InfoDescription>
+                  </FadeInUp>
+                  <FadeInUp delay={300}>
+                    <InfoDescription>I also like to spend time in local record shops, digging for the coolest old-school house music LPs. Music has always been, and will always be, a major driving force for me. On weekends, I like to attend events with friends and find sweet reprieve from my more cerebral programs of choice.</InfoDescription>
+                  </FadeInUp>
+                </InfoDescriptionWrapper>
+                <InfoIconsWrapper>
+                  <CodeIcon />
+                  <MountainIcon />
+                  <RecordIcon />
+                </InfoIconsWrapper>
               </Card>
             </Column>
           </InfoColumnWrapper>
