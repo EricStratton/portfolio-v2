1c08e4c0eb9c40a0096187be43b9604e6bad3ece
EricStratton <strattonericj@gmail.com>
2021-12-25 10:07:54 -0800

    add dataSpeed prop

--- src/components/ShapeSvg/ShapeSvgElements.js
+++ src/components/ShapeSvg/ShapeSvgElements.js
@@ -1,16 +1,21 @@
 import styled from 'styled-components';
 
-export const Svg = styled.svg.attrs({
+export const Svg = styled.svg.attrs(props => ({
   version: '1.1', 
   xmlns: 'http://www.w3.org/2000/svg', 
   xmlnsXlink: 'http://www.w3.org/1999/xlink',
-})`
-  height: 50px;
-  width: auto;
+  'data-speed': props.dataSpeed
+}))`
+  height: 5%;
+  width: 5%;
   overflow: visible;
   position: absolute;
   top: ${props => props.top};
   left: ${props => props.left};
   bottom: ${props => props.bottom};
   right: ${props => props.right};
+
+  /* top: 0;
+  left: 0; */
 `;
+
--- src/components/ShapeSvg/index.js
+++ src/components/ShapeSvg/index.js
@@ -12,23 +12,27 @@ const Circle = ({ color }) => {
 
 const Cresent = ({ color }) => {
   return (  
-  <path d='M32.36774.34317c.99276 5.63023-1.09332 11.3614-5.47227 15.03536-4.37895 3.67396-10.3855 4.73307-15.75693 2.77837C5.76711 16.2022 1.84665 11.53014.8539 5.8999l3.15139-.55567c.7941 4.50356 3.93083 8.24147 8.22772 9.8056 4.29688 1.56413 9.10275.71673 12.60554-2.2227C28.34133 9.98771 30.01045 5.4024 29.21635.89884l3.15139-.55567z' fill={color} fillRule='evenodd' />
+  <path 
+    d='M32.36774.34317c.99276 5.63023-1.09332 11.3614-5.47227 15.03536-4.37895 3.67396-10.3855 4.73307-15.75693 2.77837C5.76711 16.2022 1.84665 11.53014.8539 5.8999l3.15139-.55567c.7941 4.50356 3.93083 8.24147 8.22772 9.8056 4.29688 1.56413 9.10275.71673 12.60554-2.2227C28.34133 9.98771 30.01045 5.4024 29.21635.89884l3.15139-.55567z' 
+    fill={color} 
+    fillRule='evenodd'
+    />
   )
 }
 
 const X = ({ color }) => {
   return (
   <>
-    <line x2='25' y1='5' y2='25' stroke={color} strokeWidth='3' strokeLinecap='round' />
-    <line x1='25' y1='5' y2='25' stroke={color} strokeWidth='3' strokeLinecap='round' />
+    <line x2='20' y2='20' stroke={color} strokeWidth='3' strokeLinecap='round' />
+    <line x1='20' y2='20' stroke={color} strokeWidth='3' strokeLinecap='round' />
   </>
   )
 }
 
-const ShapeSvg = ({  top, left, bottom, right, circle, cresent, x, color}) => {
+const ShapeSvg = ({ className, dataSpeed, top, left, bottom, right, circle, cresent, x, color }) => {
   return (
     <>
-      <Svg viewBox='0 0 50 50' top={top} left={left} bottom={bottom} right={right}>
+      <Svg className={className} dataSpeed={dataSpeed} viewBox='0 0 24 25' top={top} left={left} bottom={bottom} right={right}>
         {circle &&
           <Circle color={color} />
         }
