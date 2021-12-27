6bada71ce46cd10cc71f6948122e795d72be6531
EricStratton <strattonericj@gmail.com>
2021-12-26 18:46:20 -0800

    add logic to disable scroll parallax on mobile

--- src/components/Parallax/index.js
+++ src/components/Parallax/index.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Parallax } from 'react-scroll-parallax';
 
+import { useWindowSize } from '../../hooks/useWindowSize';
+
 const ParallaxComponent = ({ children }) => {
+  const [isMobile, setIsMobile] = useState(true);
+  const size = useWindowSize();
+
+  useEffect(() => {
+    if(size[0] > 768) {
+      setIsMobile(false);
+    } else {
+      setIsMobile(true);
+    }
+  }, [size[0]]);
+
   return (
     <>
-      <Parallax y={[-20, 20]} tagOuter='div'>
+      <Parallax y={isMobile ? [-0, 0] : [-20, 20]} tagOuter='div'>
         {children}
       </Parallax>
     </>
