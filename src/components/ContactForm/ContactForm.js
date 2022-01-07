43b988909c1590642f72a7670bf873b1c08c7cb5
EricStratton <strattonericj@gmail.com>
2022-01-07 15:47:17 -0800

    build out contact form

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,21 +1,39 @@
 import styled from 'styled-components';
-import { Form, Field, ErrorMessage } from 'formik'
+import { ErrorMessage } from 'formik';
 
-export const StyledForm = styled(Form)`
+export const FormWrapper= styled.div`
+  position: relative;
   display: flex;
   flex-direction: column;
   align-items: center;
-`;
-
-export const FormWrapper = styled.div`
-  flex: 1;
   width: 100%;
+
+  form {
+    width: 100%;
+  }
+
+  textarea,
+  input {  
+    flex: 1;
+    border-radius: 10px;
+    border: none;
+    padding: 0 1em;
+    margin-bottom: 1em;
+    
+    @media screen and (min-width: ${props => props.theme.breakpoints[3]}) {
+      margin-bottom: 0;
+    }
+
+    &:focus {
+      border-bottom: 4px ${props => props.theme.colors.primary};
+      outline: 0;
+    }
+  }
 `;
 
 export const InputWrapper = styled.div`
   display: flex;
   flex-direction: column;
-  min-height: 5em;
 
   @media screen and (min-width: ${props => props.theme.breakpoints[3]}) {
     flex-direction: row;
@@ -43,6 +61,7 @@ export const FormFieldWrapper = styled.div`
   textarea {
     resize: none;
     padding-top: 0.5em;
+    overflow: hidden;
   }
 `;
 
@@ -50,19 +69,17 @@ export const FormFieldLabel = styled.label`
   margin-bottom: 0.5em;
 `;
 
-export const FormField = styled(Field)`
-  flex: 1;
-  border-radius: 10px;
-  border: none;
-  padding: 0 1em;
-  margin-bottom: 1em;
-  
-  @media screen and (min-width: ${props => props.theme.breakpoints[3]}) {
-    margin-bottom: 0;
-  }
+export const StyledErrorMessage = styled(ErrorMessage)`
+  margin-top: 1em;
+  color: red;
 `;
 
-export const StyledErrorMessage = styled(ErrorMessage)`
-
-`
+export const FormButtonWrapper = styled.div`
+  display: block;
+  
+  button {
+    display: block;
+    margin: 0 auto;
+  }
+`;
 
--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import { Formik } from 'formik';
+import { Formik, Form, Field } from 'formik';
+import axios from 'axios';
 
 import {
-  StyledForm,
   FormWrapper,
   InputWrapper,
   FormFieldWrapper,
   FormFieldLabel,
-  FormField,
-} from './ContactFormElements';
+  StyledErrorMessage,
+  FormButtonWrapper,
+} from './ContactForm';
+import { StyledButton as Button } from '../Button/Button';
 
 const ContactForm = () => {
   return (
@@ -21,39 +23,72 @@ const ContactForm = () => {
           subject: '',
           message: '',
         }}
+        validate={values => {
+          console.log('VALIDATE:', values);
+          const errors = {};
+          if (!values.email) {
+            errors.email = 'Required';
+          } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
+            errors.email = 'Invalid email address';
+          }
+          return errors;
+        }}
+        onSubmit={(values, { setSubmitting, setFieldError }) => {
+          setSubmitting(true);
+          axios({
+            method: 'POST',
+            url: 'https://formspree.io/f/xyyozobw',
+            data: values,
+          })
+            .then((response) => {
+              console.log("Response:", response); 
+            })
+            .catch((error) => {
+              setFieldError(error);
+            });
+          setTimeout(() => {
+            setSubmitting(false);
+          }, 400);
+        }}
       >
-        <StyledForm>
-          <FormWrapper>
-            <InputWrapper>
-              <FormFieldWrapper>
-                <FormFieldLabel htmlFor='firstname'>First Name</FormFieldLabel>
-                <FormField as='input' type='text' name='firstname' />
-              </FormFieldWrapper>
-              <FormFieldWrapper>
-                <FormFieldLabel htmlFor='lastname'>Last Name</FormFieldLabel>
-                <FormField as='input' type='text' name='lastname' />
-              </FormFieldWrapper>
-            </InputWrapper>
-            <InputWrapper>
-              <FormFieldWrapper>
-                <FormFieldLabel htmlFor='email'>Email</FormFieldLabel>
-                <FormField as='input' type='email' name='email' />
-              </FormFieldWrapper>
-            </InputWrapper>
-            <InputWrapper>
-              <FormFieldWrapper>
-                <FormFieldLabel htmlFor='subject'>Subject</FormFieldLabel>
-                <FormField as='input' type='text' name='subject' />
-              </FormFieldWrapper>
-            </InputWrapper>
-            <InputWrapper>
-              <FormFieldWrapper>
-                <FormFieldLabel htmlFor='message'>Message</FormFieldLabel>
-                <FormField as='textarea' rows={5} name='message' />
-              </FormFieldWrapper>
-            </InputWrapper>
-          </FormWrapper>
-        </StyledForm>
+        {({ isSubmitting, isValid }) => (
+            <FormWrapper>
+              <Form>
+                <InputWrapper>
+                  <FormFieldWrapper>
+                    <FormFieldLabel htmlFor='firstname'>First Name</FormFieldLabel>
+                    <Field as='input' type='text' name='firstname' />
+                  </FormFieldWrapper>
+                  <FormFieldWrapper>
+                    <FormFieldLabel htmlFor='lastname'>Last Name</FormFieldLabel>
+                    <Field as='input' type='text' name='lastname' />
+                  </FormFieldWrapper>
+                </InputWrapper>
+                <InputWrapper>
+                  <FormFieldWrapper>
+                    <FormFieldLabel htmlFor='email'>Email*</FormFieldLabel>
+                    <Field as='input' type='email' name='email' />
+                    <StyledErrorMessage name='email' component='div' />
+                  </FormFieldWrapper>
+                </InputWrapper>
+                <InputWrapper>
+                  <FormFieldWrapper>
+                    <FormFieldLabel htmlFor='subject'>Subject</FormFieldLabel>
+                    <Field as='input' type='text' name='subject' />
+                  </FormFieldWrapper>
+                </InputWrapper>
+                <InputWrapper>
+                  <FormFieldWrapper>
+                    <FormFieldLabel htmlFor='message'>Message</FormFieldLabel>
+                    <Field as='textarea' rows={5} name='message' />
+                  </FormFieldWrapper>
+                </InputWrapper>
+                <FormButtonWrapper>
+                  <Button type='submit' disabled={!isValid || isSubmitting}>Submit</Button>
+                </FormButtonWrapper>
+                </Form>
+            </FormWrapper> 
+        )}
       </Formik>
     </>
   )
