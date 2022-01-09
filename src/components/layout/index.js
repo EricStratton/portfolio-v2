27f53aad759e060f22a398b21f52a063006c1b87
EricStratton <strattonericj@gmail.com>
2022-01-09 11:35:24 -0800

    add useWindowSize and sidebar

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
+import Sidebar from './Sidebar';
 import Navbar from './Navbar';
 import Footer from './Footer';
+import { useWindowSize } from './../../hooks/useWindowSize';
 
 const Layout = ({ children }) => {
+  const [isMobile, setIsMobile] = useState(false);
+  const size = useWindowSize();
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
+
+  useEffect(() => {
+    if(size[0] < 768) {
+      setIsMobile(true);
+    } else {
+      setIsMobile(false);
+    }
+  }, [size]);
+
   return (
     <>
-      <Navbar />
+      { isMobile && <Sidebar click={click} handleClick={handleClick} /> }
+      <Navbar isMobile={isMobile} click={click} handleClick={handleClick} />
         {children}
       <Footer />
     </>
