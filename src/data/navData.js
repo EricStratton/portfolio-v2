156acd0cad696bcb81d49cfd70815b4508942c02
EricStratton <strattonericj@gmail.com>
2022-01-09 11:34:29 -0800

    add navData file

---
+++ src/data/navData.js
@@ -0,0 +1,18 @@
+export const navData = [
+  {
+    title: 'About Me',
+    path: 'about'
+  },
+  {
+    title: 'Experience',
+    path: 'experience'
+  },
+  {
+    title: 'Projects',
+    path: 'projects'
+  },
+  {
+    title: 'Contact Me',
+    path: '/contact'
+  }
+]
