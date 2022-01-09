9024043e1bfa6086139321826b5fc7c07494a5ba
EricStratton <strattonericj@gmail.com>
2022-01-09 09:30:02 -0800

    add text and scroll to top

--- src/components/layout/Footer/FooterElements.js
+++ src/components/layout/Footer/FooterElements.js
@@ -4,4 +4,11 @@ export const FooterContainer = styled.div`
   background: ${props => props.theme.colors.dark};
   display: flex;
   height: 5em;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const FooterText = styled.p`
+  color: ${props => props.theme.colors.body};
+  cursor: pointer;
 `;
--- src/components/layout/Footer/index.js
+++ src/components/layout/Footer/index.js
@@ -1,14 +1,23 @@
 import React from 'react';
 
 import {
-  FooterContainer
+  FooterContainer, 
+  FooterText
 } from './FooterElements';
 
 const Footer = () => {
+
+  const handleScrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    })
+  }
+
   return (
     <>
       <FooterContainer>
-        
+        <FooterText onClick={handleScrollToTop}>Eric Stratton @ 2022</FooterText>
       </FooterContainer>
     </>
   )
