ad700fa0112a917924bc6c83f2c8c605145c6e24
EricStratton <strattonericj@gmail.com>
2022-01-09 09:29:32 -0800

    implement scrolltotop component

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from './components/layout';
 import Home from './pages/Home';
 import Contact from './pages/Contact';
+import ScrollToTop from './components/ScrollToTop';
 
 function App() {
   return (
     <>
     <BrowserRouter>
       <Layout>
+        <ScrollToTop />
         <Routes>
           <Route path='/contact' element={<Contact />} />
           <Route path='/' element={<Home />} />
