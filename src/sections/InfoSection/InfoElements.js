13e46ce7984425f557e95fe3d505202788574064
EricStratton <strattonericj@gmail.com>
2022-01-17 20:28:32 -0800

    img size fix

--- src/sections/InfoSection/InfoElements.js
+++ src/sections/InfoSection/InfoElements.js
@@ -27,8 +27,8 @@ export const InfoDescription = styled.p`
 `;
 
 export const InfoImgWrapper = styled.figure`
-  width: 21em;
-  height: 21em;
+  width: 18em;
+  height: 18em;
   background: ${props => props.theme.colors.primary};
   border-radius: 50%;
   display: block;
@@ -37,9 +37,29 @@ export const InfoImgWrapper = styled.figure`
   padding-top: 0.5em;
   position: relative;
   box-shadow: 0 5px 20px 0 rgb(69 67 96 / 10%);
+  
+  @media screen and (min-width: ${props => props.theme.breakpoints[1]}) {
+    width: 19em;
+    height: 19em;
+  }
+
+  @media screen and (min-width: ${props => props.theme.breakpoints[2]}) {
+    width: 20em;
+    height: 20em;
+  }
+
+  @media screen and (min-width: ${props => props.theme.breakpoints[3]}) {
+    width: 21em;
+    height: 21em;
+  }
 `;
 
 export const InfoImg = styled.img`
   position: absolute;
-  top: 5;
+  top: 25px;
+  left: -20px;
+  
+  @media screen and (min-width: ${props => props.theme.breakpoints[3]}) {
+    left: 5px;
+  }
 `;
