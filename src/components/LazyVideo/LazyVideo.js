125e8fc2029e028e21c07d0481ce25a773db91db
EricStratton <strattonericj@gmail.com>
2022-01-30 16:13:42 -0800

    add display block property

--- src/components/LazyVideo/LazyVideo.js
+++ src/components/LazyVideo/LazyVideo.js
@@ -3,6 +3,8 @@ import styled from 'styled-components';
 export const LazyVideoWrapper = styled.div`
   object-fit: contain;
   width: fit-content;
+  display: block;
+  margin: 0 auto;
 `;
 
 export const Video = styled.video`
